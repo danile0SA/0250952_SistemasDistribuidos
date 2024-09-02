5734bf29f5451de1e083b58e03192e2e7cf04e39
danile0SA <0250952@up.edu.mx>
2024-09-02 15:38:27 -0600

    Store

---
+++ Internal/Server/stores.go
@@ -0,0 +1,91 @@
+package server
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+
+	writtenBytes, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	writtenBytes += lenWidth
+	s.size += uint64(writtenBytes)
+
+	return uint64(writtenBytes), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	sizeBuf := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(sizeBuf, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	size := enc.Uint64(sizeBuf)
+	data := make([]byte, size)
+	if _, err := s.File.ReadAt(data, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.File.Close()
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+
+return &store {
+	File: f,
+	size: size,
+	buf: bufio.NewWriter(f)
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
 
 	server "github.com/danile0SA/0250952_SistemasDistribuidos/Project_Module/Internal/Server"
 )
@@ -9,4 +12,38 @@ import (
 func main() {
 	srv := server.NewHTTPServer(":8080")
 	log.Fatal(srv.ListenAndServe())
+
+	// Ejemplo de inicialización del store
+	f, err := os.OpenFile("logger.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+	}
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}
+
+	// Ejemplo de Append
+	data := []byte("Hola, mundo!")
+	_, pos, err := s.Append(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ejemplo de Read
+	readData, err := s.Read(pos)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(readData))
+
+	// Cerrar el store
+	if err := s.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
